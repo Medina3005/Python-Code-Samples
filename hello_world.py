d975be7ca71e1bdf9b2a5a74a3764cc188b0d5bb
Medina <kubanycbekovam87@gmail.com>
2025-02-06 19:23:30 -0600

    Initial commit - Adding hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,130 @@
+# hello_world.py 
+# Medina Kubanychbekova
+# Date: Feb 6, 2025
+# Problem 1 – Print "Hello World"
+
+
+# a = "Hello World"
+# print(a)
+
+
+# print("Hello World")
+# I was trying to put two ways printing the Hello World output. In the first my example i give a variable first and then print the variable that gives Hello World output. 
+# In the second part I just print the word "Hello World" that was the easiest way.
+
+
+
+# a = "Hello World"
+# print(a)
+
+
+# print("Hello World")
+# I was trying to put two ways printing the Hello World output. In the first my example i give a variable first and then print the variable that gives Hello World output.
+# In the second part I just print the word "Hello World" that was the easiest way.
+
+
+
+
+
+
+# Medina Kubanychbekova
+# Date: Feb 6, 2025
+# Problem 2 – Ask for the user's name and greet them
+
+
+#!/usr/bin/env python
+# username = input("Please enter your name:")
+# user = "Medina"
+# if username == user:
+#     print("Welcome to the system")
+# else:
+#     print("Access denied")
+
+
+# In my second part i just put my name in the variable and used if and else conditionals. I give only one variable - my name.When I run the code, first they asked me to
+# enter the name, if i enter the correct name it will give the output of "Welcome to the system".
+# But if i provide the wrong name, it will print Access denied. I just put only my name because for example in my computer i also have a password and
+# when i put the correct name it will autamitaccly allow me to enter to the system.
+
+
+# Medina Kubanychbekova
+# Date: Feb 6, 2025
+# Problem 3 – Greet only specific users
+# username = input("Please enter your name:")
+# user1 = "Medina"
+# user2 = "Nathan Braun"
+# if username == user1:
+#     print("Welcome to the system")
+# elif username == user2:
+#     print("Welcome to the system")
+# else:
+#     print("Access denied")
+
+
+    # As it was in the instructions, i added only myself and instructor. My code work. When I enter my name or instructor anme it greets us.
+
+
+
+
+# import math
+
+
+# radius = float(input("Enter the radius of the circle: "))
+# area = math.pi * radius ** 2
+# print("The area of the circle is:", area)
+
+
+# Medina Kubanychbekova
+# Date: Feb 6, 2025
+# This program calculates the area of a circle based on user input.
+
+
+
+
+
+
+# Medina Kubanychbekova
+# Date: Feb 6, 2025
+# This program calculates the miles per gallon (MPG) of a car.
+
+
+# Prompt user for input
+# miles = float(input("Enter the number of miles driven: "))
+# gallons = float(input("Enter the number of gallons used: "))
+
+
+# # Compute MPG, ensuring no division by zero
+# if gallons > 0:
+#     mpg = miles / gallons
+#     print("Your car's fuel efficiency is MPG.", mpg)
+# else:
+#     print("Error: Gallons used cannot be zero or negative.")
+
+
+
+
+
+
+# Medina Kubanychbekova
+# Date: Feb 6, 2025
+# Problem 6 – Convert Fahrenheit to Celsius
+# fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+# celsius = (fahrenheit - 32) * 5 / 9
+# print("Temperature in Celsius:", celsius)
+
+
+
+
+# Medina Kubanychbekova
+# Date: Feb 6, 2025
+# Problem 7 – Find return day of the week
+# start_day = int(input("Enter starting day number:"))
+# stay_length = int(input("Enter number of nights: "))
+
+
+# return_day = (start_day + stay_length) % 7
+# days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+
+# print("You will return on:", days,return_day)
+
